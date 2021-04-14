91082a2589816064118a0230106c30fb26fec264
felicitasmock <81470249+felicitasmock@users.noreply.github.com>
2021-04-14 13:04:35 +0200

    added details on details layer

--- backlog/backlog.js
+++ backlog/backlog.js
@@ -44,7 +44,7 @@ function showBacklogTask() {
     for (let i = 0; i < backlogTasks.length; i++) {
         let task = backlogTasks[i];
         taskContainer.innerHTML += `
-        <div onclick="openTaskDetail()" class="bl-task">
+        <div onclick="openTaskDetail(${i})" class="bl-task">
             <div id="color${i}" class="color-category"></div>
             <div class="bl-name">${task['name']}</div>
             <div id="category${i}" class="bl-category">${task['category']}</div>
@@ -79,13 +79,32 @@ function showCategory() {
  * function to open Details of Tasks
  */
 
-function openTaskDetail() {
+function openTaskDetail(i) {
     let detailLayer = document.getElementById('taskDetails');
-    
+  
+    let title = backlogTasks[i]['title'];
+    let blName = backlogTasks[i]['name'];
+    let category = backlogTasks[i]['category'];
+    let description = backlogTasks[i]['description'];
+    let date = backlogTasks[i]['date'];
+    let prio = backlogTasks[i]['urgency'];
+
     detailLayer.innerHTML = `
     <div class="details-layer-background">
         <div class="details-layer">
         <span onclick="closeTaskDetail()">Schließen</span>
+        <div>Titel ${title}
+        </div>
+        <div>Name ${blName}
+        </div>
+        <div>Kategorie ${category}
+        </div>
+        <div>Description ${description}
+        </div>
+        <div>Date ${date}
+        </div>
+        <div>Prio ${prio}
+        </div>
         </div>
     </div>
     `;
